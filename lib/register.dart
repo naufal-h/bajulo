20ab0bf13ca4d3d957b78f8a66adb58dc0db83e7
naufal-h <naufal.humam-2021@fst.unair.ac.id>
2023-03-08 13:13:31 +0700

    register, icons + benerin login

--- lib/register.dart
+++ lib/register.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
-import 'package:bajulo/register.dart';
 import 'package:bajulo/main.dart';
+import 'package:bajulo/login fix.dart';
 
-class loginfix extends StatelessWidget {
-  const loginfix({super.key});
+class registerPage extends StatelessWidget {
+  const registerPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -34,17 +34,17 @@ class loginfix extends StatelessWidget {
         child: TextButton(
           onPressed: () {
             Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => registerPage()));
+                .push(MaterialPageRoute(builder: (context) => loginfix()));
           },
           style: TextButton.styleFrom(
-            foregroundColor: Colors.black.withOpacity(0.1),
+            foregroundColor: Colors.white.withOpacity(0.1),
           ),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'Dont have an account?',
+                'Already have an account?',
                 style: TextStyle(
                   color: Colors.white.withOpacity(0.7),
                   fontSize: 14,
@@ -52,7 +52,7 @@ class loginfix extends StatelessWidget {
                 ),
               ),
               Text(
-                ' Sign Up',
+                'Sign In',
                 style: TextStyle(
                   color: Theme.of(context).primaryColor,
                   fontSize: 14,
@@ -71,7 +71,7 @@ class loginfix extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(top: 20, bottom: 12),
             child: Text(
-              'Welcome back to BajuLo!',
+              'Welcome to Bajulo!',
               style: TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.w700,
@@ -83,13 +83,88 @@ class loginfix extends StatelessWidget {
           Container(
             margin: EdgeInsets.only(bottom: 32),
             child: Text(
-              'Silahkan masukan data untuk login',
+              'Silahkan masukkan data diri anda untuk mendaftar',
               style: TextStyle(
                   color: Colors.white.withOpacity(0.7),
                   fontSize: 12,
                   height: 150 / 100),
             ),
           ),
+          Container(
+            margin: EdgeInsets.only(top: 20, bottom: 12),
+            child: Text(
+              'Full Name',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'poppins',
+                fontSize: 16,
+              ),
+            ),
+          ),
+          TextField(
+            autofocus: false,
+            decoration: InputDecoration(
+              hintText: 'Name',
+              prefixIcon: Container(
+                padding: EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/user.svg',
+                    color: Colors.black),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Theme.of(context).primaryColor, width: 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              fillColor: Colors.white,
+              filled: true,
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            margin: EdgeInsets.only(top: 20, bottom: 12),
+            child: Text(
+              'Username',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'poppins',
+                fontSize: 16,
+              ),
+            ),
+          ),
+          TextField(
+            autofocus: false,
+            decoration: InputDecoration(
+              hintText: 'Username',
+              prefixIcon: Container(
+                padding: EdgeInsets.all(12),
+                child: Text('@',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.black)),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Theme.of(context).primaryColor, width: 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              fillColor: Colors.white,
+              filled: true,
+            ),
+          ),
+          SizedBox(height: 16),
           Container(
             margin: EdgeInsets.only(top: 20, bottom: 12),
             child: Text(
@@ -103,6 +178,7 @@ class loginfix extends StatelessWidget {
           ),
           TextField(
             autofocus: false,
+            keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
               hintText: 'Email',
               prefixIcon: Container(
@@ -167,30 +243,56 @@ class loginfix extends StatelessWidget {
               ),
             ),
           ),
+          SizedBox(height: 16),
           Container(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                'Forgot Password ?',
-                style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.white.withOpacity(0.5),
-                    fontWeight: FontWeight.w700),
-              ),
-              style: TextButton.styleFrom(
-                foregroundColor:
-                    Theme.of(context).primaryColor.withOpacity(0.1),
+            margin: EdgeInsets.only(top: 20, bottom: 12),
+            child: Text(
+              'Repeat Password',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'poppins',
+                fontSize: 16,
               ),
             ),
           ),
+          TextField(
+            autofocus: false,
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Repeat Password',
+              prefixIcon: Container(
+                padding: EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/lock.svg',
+                    color: Colors.black),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black, width: 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide:
+                    BorderSide(color: Theme.of(context).primaryColor, width: 1),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              fillColor: Colors.white,
+              filled: true,
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset('assets/icons/eye.svg',
+                    color: Colors.black),
+              ),
+            ),
+          ),
+          SizedBox(height: 24),
           ElevatedButton(
             onPressed: () {
               Navigator.of(context)
                   .push(MaterialPageRoute(builder: (context) => myApp()));
             },
             child: Text(
-              'Sign In',
+              'Sign Up',
               style: TextStyle(
                   color: Colors.black,
                   fontWeight: FontWeight.w600,
@@ -200,6 +302,47 @@ class loginfix extends StatelessWidget {
             style: ElevatedButton.styleFrom(
               padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
               backgroundColor: Theme.of(context).primaryColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16)),
+              elevation: 0,
+              shadowColor: Colors.transparent,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(vertical: 16),
+            child: Text(
+              'or continue with',
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.white.withOpacity(0.5),
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/google.svg',
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 16),
+                  child: Text(
+                    'Continue with Google',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 36, vertical: 12),
+              backgroundColor: Colors.white,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(16)),
               elevation: 0,
